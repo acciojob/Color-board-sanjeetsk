3d20e6fb5042fd27ba4ceb98286754042f846e35
Sanjeet Kumar <96577772+sanjeetsk@users.noreply.github.com>
2024-12-14 23:53:45 +0530

    commit

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function changeColor(element) {
 }
 
 function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+    const r = Math.floor(Math.random() * 128);
+    const g = Math.floor(Math.random() * 128);
+    const b = Math.floor(Math.random() * 128);
     return `rgb(${r}, ${g}, ${b})`;
 }
