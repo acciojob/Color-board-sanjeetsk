817933580c3561317bc88b05736586785f8189a4
Sanjeet Kumar <96577772+sanjeetsk@users.noreply.github.com>
2024-12-14 23:27:29 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const container = document.getElementById('colorBoard');
+
+// create 800 boxes dynamically
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    // add event listener for hover class
+    square.addEventListener('mouseover', () => changeColor(square));
+    container.appendChild(square);
+}
+
+function changeColor(element) {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    element.style.backgroundColor = randomColor;
+
+    //Add the active class for smooth fade out
+    element.classList.add('active');
+
+    //Remove active class after 1sec
+    setTimeout(() => {
+        element.classList.remove('active');
+    }, 1000);
+}
